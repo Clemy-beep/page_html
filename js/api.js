0afd25841e3ab34eb4b104149f3aedc491e7a90f
Clemy-beep <clementine.digny@gmail.com>
2021-11-07 13:21:35 +0100

    weather api & page

--- js/api.js
+++ js/api.js
@@ -35,3 +35,63 @@ function getCities() {
         }
     });
 }
+
+function getWeather() {
+    const apiKey = "91e21e1299291a6b6f8d752fca598814";
+    let cityName = document.getElementById("cities").value;
+    $.ajax({
+        type: "GET",
+        url: "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey,
+        data: "json",
+        success: function(response) {
+            let weather = response["weather"][0]["description"];
+            let weatherTranslation = "";
+            switch (weather) {
+                case "clear sky":
+                    weatherTranslation = "ensoleillé";
+                    document.getElementById("weather-icon").className = "fas fa-sun";
+                    break;
+                case "few clouds":
+                    weatherTranslation = "peu nuageux";
+                    document.getElementById("weather-icon").className = "fas fa-cloud-sun";
+                    break;
+                case "scattered clouds":
+                    weatherTranslation = "nuageux";
+                    document.getElementById("weather-icon").className = "fas fa-cloud";
+                    break;
+                case "broken clouds":
+                    weatherTranslation = "fortement nuageux";
+                    document.getElementById("weather-icon").className = "fas fa-cloud";
+                    break;
+                case "shower rain":
+                    weatherTranslation = "bruineux";
+                    document.getElementById("weather-icon").className = "fas fa-cloud-rain";
+                    break;
+                case "rain":
+                    weatherTranslation = "pluvieux";
+                    document.getElementById("weather-icon").className = "fas fa-cloud-showers-heavy";
+                    break;
+                case "thunderstorm":
+                    weatherTranslation = "orageux";
+                    document.getElementById("weather-icon").className = "fas fa-bolt";
+                    break;
+                case "snow":
+                    weatherTranslation = "neige";
+                    document.getElementById("weather-icon").className = "fas fa-snowflake";
+                    break;
+                case "mist":
+                    weatherTranslation = "brouillard";
+                    document.getElementById("weather-icon").className = "fas fa-smog";
+                    break;
+                default:
+                    weatherTranslation = "indéfini";
+                    document.getElementById("weather-icon").className = "";
+                    break;
+            }
+            document.getElementById("weather").innerHTML = "Le temps à " + cityName + "  est : " + weatherTranslation;
+        },
+        error: function(err) {
+            console.log("error", err)
+        }
+    });
+}
